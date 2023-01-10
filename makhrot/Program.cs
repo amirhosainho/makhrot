b02ba1e3454994898d17ba1af01395a5da0fcbc0
Amir Hosain <Amir Hosain@DESKTOP-B550F2F>
2023-01-10 09:23:22 -0800

    Add project files.

---
+++ makhrot/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace makhrot
+{
+    internal class Program
+    {
+        private const double p = 3.14;
+        private const double n = 0.33;
+
+        static void Main(string[] args)
+        {
+            float h, r, v;
+            Console.Write(" plase enter h : " );
+            h = float.Parse(Console.ReadLine());
+            Console.Write(" plase enter r : " );
+            r = float.Parse(Console.ReadLine());
+            v =(float)(n * p * ((float)Math.Pow(r, 2)) * h);
+            Console.WriteLine(" hajm mekhrot = "+v);
+            Console.ReadKey();
+
+
+        }
+    }
+}
